5ddca813334796fdaa5d3d5c7e707743a54fdecf
Daniil <Zest005@gmail.com>
2025-03-01 04:49:47 +0200

    Squashed commit of the following:
    commit 63298b054a7aac6ac3b1b1d137871c974a2752a7
    Merge: aa6fccd 00e1623
    Author: Daniil Konovalenko <81220254+Zest005@users.noreply.github.com>
    Date:   Sat Mar 1 04:35:52 2025 +0200
    
        Merge pull request #2 from Zest005/alexander-karvieshkin
    
        Create EF migration
    
    commit 00e162336922f83c7cb77134a5a0bf389f14863d
    Author: Amirsoon <142514990+Amirsoon@users.noreply.github.com>
    Date:   Sat Mar 1 04:33:29 2025 +0200
    
        Create EF migration
    
    commit ac3241f29805e891b6a7e0da438266235115adc3
    Author: Amirsoon <142514990+Amirsoon@users.noreply.github.com>
    Date:   Sat Mar 1 04:21:10 2025 +0200
    
        Delete unnecessary controller
    
    commit aa6fccd8526be95a534e7edba3d8253df9b2f015
    Merge: 06be702 94e1c9a
    Author: Amirsoon <142514990+Amirsoon@users.noreply.github.com>
    Date:   Sat Mar 1 04:16:47 2025 +0200
    
        Merge pull request #1 from Zest005/daniil-konovalenko
    
        Create API proj

---
+++ VibeHub/Core/Models/Music.cs
@@ -0,0 +1,10 @@
+using Core.Base;
+
+namespace Core.Models;
+
+public class Music : BaseEntity
+{
+    public string Title { get; set; }
+    public string Artist { get; set; }
+    public string Filename { get; set; }
+}
---
+++ VibeHub/Core/Models/Room.cs
@@ -0,0 +1,12 @@
+using Core.Base;
+
+namespace Core.Models;
+
+public class Room : BaseEntity
+{
+    public string Code { get; set; }
+    public short UserCount { get; set; }
+    public bool Availability { get; set; }
+    public Guid OwnerId { get; set; }
+    public List<Guid>? MusicIds { get; set; }
+}
