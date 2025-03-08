6207596dc21a213e761ac45262052929fbcc7db4
Amirsoon <142514990+Amirsoon@users.noreply.github.com>
2025-03-09 01:58:45 +0200

    Added new Table to DB

--- VibeHub/Core/Models/User.cs
+++ VibeHub/Core/Models/User.cs
@@ -8,5 +8,7 @@ public class User : BaseEntity
     public string Email { get; set; }
     public string Nickname { get; set; }
     public string Password { get; set; }
+    public bool IsAdmin { get; set; } = false;
+    
     public Room? Room { get; set; }
 }
